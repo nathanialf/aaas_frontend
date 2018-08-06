51543ba57840c58649da7a983770f3dcc4140640
nathanialf <nathanial.fine@gmail.com>
2018-08-06 15:22:54 -0700

    Informs user of both success and failure when trying to add a user

--- controllers/TEST-addUserController.js
+++ controllers/TEST-addUserController.js
@@ -1,6 +1,7 @@
 app.controller('TEST-addUserController', function($scope, $rootScope, $location, $http)
 {
-    $scope.showMsg = false;
+    $scope.showSuccess = false;
+    $scope.showDanger = false;
     $scope.message = "";
     $scope.addUser = function(){
         var uri = aaasBackendEndpoint + "/user";
@@ -16,10 +17,13 @@ app.controller('TEST-addUserController', function($scope, $rootScope, $location,
                 user_active:    1
             }
         }).then(function success(response){
-            console.log("SUCCESS");
+            $scope.showSuccess = true;
+            $scope.message = "Added " + response.data.user_fname + " " + response.data.user_lname;
+            console.log($scope.message);
+            console.log(response.data);
         }, function error(response){
-            $scope.showMsg = true;
-            $scope.message = "Adding User Failed";
+            $scope.showDanger = true;
+            $scope.message = "Failed to add " + $scope.user_fname + " " + $scope.user_lname;
             console.log($scope.message);
         });
     }
