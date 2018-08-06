e91bd83f2f446dbf1919c70dd1f4faeff17bf1aa
nathanialf <nathanial.fine@gmail.com>
2018-08-06 08:42:55 -0700

    Adding User Test form is functional. Beginning creating the style of the application.

--- controllers/TEST-addUserController.js
+++ controllers/TEST-addUserController.js
@@ -1,3 +1,26 @@
 app.controller('TEST-addUserController', function($scope, $rootScope, $location, $http)
 {
+    $scope.showMsg = false;
+    $scope.message = "";
+    $scope.addUser = function(){
+        var uri = aaasBackendEndpoint + "/user";
+        $scope.showMsg = false;
+        $http({
+            url:        uri,
+            method:     'POST',
+            data:   {
+                user_email:     $scope.user_email,
+                user_fname:     $scope.user_fname,
+                user_lname:     $scope.user_lname,
+                user_pass:      $scope.user_pass,
+                user_active:    1
+            }
+        }).then(function success(response){
+            console.log("SUCCESS");
+        }, function error(response){
+            $scope.showMsg = true;
+            $scope.message = "Adding User Failed";
+            console.log($scope.message);
+        });
+    }
 });
