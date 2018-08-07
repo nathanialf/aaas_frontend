d9990d4f14db664b491623c950f56c08b69bf439
nathanialf <nathanial.fine@gmail.com>
2018-08-07 08:27:01 -0700

    refactored user attributes to match the backend changes. Made sure it didn't show a success and error message at the same time

--- controllers/TEST-addUserController.js
+++ controllers/TEST-addUserController.js
@@ -3,6 +3,7 @@ app.controller('TEST-addUserController', function($scope, $rootScope, $location,
     $scope.showSuccess = false;
     $scope.showDanger = false;
     $scope.message = "";
+
     $scope.addUser = function(){
         var uri = aaasBackendEndpoint + "/user";
         $scope.showMsg = false;
@@ -10,21 +11,24 @@ app.controller('TEST-addUserController', function($scope, $rootScope, $location,
             url:        uri,
             method:     'POST',
             data:   {
-                user_email:     $scope.user_email,
-                user_fname:     $scope.user_fname,
-                user_lname:     $scope.user_lname,
-                user_pass:      $scope.user_pass,
-                user_active:    1
+                email:     $scope.user_email,
+                fname:     $scope.user_fname,
+                lname:     $scope.user_lname,
+                pass:      $scope.user_pass,
+                active:    1
             }
         }).then(function success(response){
             $scope.showSuccess = true;
-            $scope.message = "Added " + response.data.user_fname + " " + response.data.user_lname;
+            $scope.showDanger = false;
+            $scope.message = "Added " + response.data.fname + " " + response.data.lname;
             console.log($scope.message);
             console.log(response.data);
         }, function error(response){
+            $scope.showSuccess = false;
             $scope.showDanger = true;
             $scope.message = "Failed to add " + $scope.user_fname + " " + $scope.user_lname;
             console.log($scope.message);
+            console.log(response);
         });
     }
 });
